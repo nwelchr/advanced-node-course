7989a8eb667c155ae877e5c50364a67ccf3b6665
nwelchr <nwelchr@gmail.com>
2018-07-14 16:14:33 -0700

    Install pm2

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require('express');
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+// first time around cluster.isMaster will be true
+// so we call .fork which creates a worker thread
+// which runs index.js again but where cluster.isMaster
+// is false :)
+if (cluster.isMaster) {
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  const crypto = require('crypto');
+  const express = require('express');
+  const app = express();
+
+  app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('That was fast!');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send({ wow: 'baby' });
-});
-
-app.listen(3000);
